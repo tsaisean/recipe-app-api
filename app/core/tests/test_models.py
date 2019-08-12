27085372aeead34c044b2ca5636bfd72175029e1
Sean Tsai <sean.tsai@carousell.com>
2019-08-12 16:10:28 +0800

    Add superuser function

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -14,3 +14,24 @@ class ModelTests(TestCase):
 
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        email = "test@carousell.COM"
+        user = get_user_model().objects.create_user(
+            email, "1234567"
+        )
+
+        self.assertEqual(user.email, email.lower())
+
+    def test_new_user_invalid_email(self):
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None, 'test123')
+
+    def test_create_new_superuser(self):
+        user = get_user_model().objects.create_superuser(
+            "test@carousell.com",
+            "1234567"
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
